de81fc88d2f581ee3e09dee50a2b487b0a8f250a
AbhishekVarma11 <abhishekgollapalli11@gmail.com>
2023-07-09 12:02:53 +0530

    third commit

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 def api_home(request,*args,**kwargs):
+    body=request.body 
+    print(body)
     return JsonResponse({"message":"Hi there this is your first Djnago API response"})
