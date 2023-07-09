734cccf313ce1babd6eaa469ada9bbab26413600
AbhishekVarma11 <abhishekgollapalli11@gmail.com>
2023-07-09 12:18:30 +0530

    fourth commit

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+import json
 def api_home(request,*args,**kwargs):
     body=request.body 
-    print(body)
-    return JsonResponse({"message":"Hi there this is your first Djnago API response"})
+    data={}
+    try:
+        data=json.loads(body)
+    except:
+        pass
+    print(data)
+    data['headers']=dict(request.headers)
+    data['content_type']=request.content_type
+    
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -7,4 +7,4 @@ get_response=requests.get(endpoint,params={'abc':123},json={"message":"hello abh
 
 #print(get_response.text)
 #print(get_response.status_code)
-print(get_response.json())
+#print(get_response.json())
