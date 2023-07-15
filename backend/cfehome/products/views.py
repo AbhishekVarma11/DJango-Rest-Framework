8776538980d80bb2f024867247c17fcad0731ff1
AbhishekVarma11 <abhishekgollapalli11@gmail.com>
2023-07-15 13:03:33 +0530

    use the detailview from rest to retrieve the data

--- backend/cfehome/products/views.py
+++ backend/cfehome/products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics 
+
+from .models import product
+from .serializers import ProductSerializer
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset=product.objects.all()
+    serializer_class=ProductSerializer
+    #lookup_field='pk'
+    
+product_detail_view=ProductDetailAPIView.as_view()
