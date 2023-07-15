660db7a900060fb806dc7a3c9ccc1b9bd429f2ad
AbhishekVarma11 <abhishekgollapalli11@gmail.com>
2023-07-15 12:49:34 +0530

    use the seriailizers for product post

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -20,13 +20,22 @@ from rest_framework.decorators import api_view
 
 from rest_framework.response import Response
 from products.serializers import ProductSerializer
-@api_view(["GET"])
+# @api_view(["GET"])
+# def api_home(request,*args,**kwargs):
+#     instance=product.objects.all().order_by("?").first()
+#     data={}
+#     if instance:
+#         #data=model_to_dict(model_data,fields=['id','title','price'])
+#         data=ProductSerializer(instance).data
+#     return Response(data)
+
+@api_view(["POST"])
 def api_home(request,*args,**kwargs):
-    instance=product.objects.all().order_by("?").first()
-    data={}
-    if instance:
-        #data=model_to_dict(model_data,fields=['id','title','price'])
-        data=ProductSerializer(instance).data
-    return Response(data)
+   serializer=ProductSerializer(data=request.data)
+   if serializer.is_valid(raise_exception=True):
+    #    instance=serializer.save()
+    #    print(instance)
+       return Response(serializer.data)
+   return Response({"invalid":"not good data"},status=400)
 
 
--- consumer/basic.py
+++ consumer/basic.py
@@ -2,7 +2,8 @@ import requests
 
 endpoint="http://127.0.0.1:8000/api/"
 
-get_response=requests.get(endpoint,params={'abc':123},json={"message":"hello abhi"})
+get_response=requests.get(endpoint,params={'abc':123},json={"title":"hello abhi"})
+print(get_response)
 
 
 #print(get_response.text)
