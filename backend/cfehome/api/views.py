306e40ace010803339d5914e07719a02e485acaa
AbhishekVarma11 <abhishekgollapalli11@gmail.com>
2023-07-15 11:39:31 +0530

    added first api for Get

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.forms.models import model_to_dict
 import json
 from products.models import product
-def api_home(request,*args,**kwargs):
+# def api_home(request,*args,**kwargs):
     
+#     model_data=product.objects.all().order_by("?").first()
+#     data={}
+#     if model_data:
+#         data=model_to_dict(model_data,fields=['id','title'])
+#         # data['id']=model_data.id
+#         # data['title']=model_data.title
+#         # data['content']=model_data.content 
+#         # data['price']=model_data.price 
+            
+#     return JsonResponse(data)
+
+from rest_framework.decorators import api_view
+
+from rest_framework.response import Response
+
+@api_view(["GET"])
+def api_home(request,*args,**kwargs):
     model_data=product.objects.all().order_by("?").first()
     data={}
     if model_data:
-        data['id']=model_data.id
-        data['title']=model_data.title
-        data['content']=model_data.content 
-        data['price']=model_data.price 
-            
-    return JsonResponse(data)
+        data=model_to_dict(model_data,fields=['id','title','price'])
+        return Response(data)
+
+
